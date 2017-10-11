d978f4215f3832973424bb1c4690422940a78605
Tõnis Kasekamp <tonis.kasekamp@salemove.com>
2017-10-11 17:10:38 +0300

    Add generic result table
    Shows reg and class stats

---
+++ src/components/validation/ColumnHelper.js
@@ -0,0 +1,49 @@
+/**
+ * Created by tonis.kasekamp on 10/11/17.
+ */
+
+export const classColumns = [
+  {
+    type: 'string',
+    label: 'uuid',
+  },
+  {
+    type: 'string',
+    label: 'run',
+  },
+  {
+    type: 'number',
+    label: 'fmeasure',
+  },
+  {
+    type: 'number',
+    label: 'acc',
+  },
+  {
+    type: 'number',
+    label: 'auc',
+  }
+];
+
+export const regColumns = [
+  {
+    type: 'string',
+    label: 'uuid',
+  },
+  {
+    type: 'string',
+    label: 'run',
+  },
+  {
+    type: 'number',
+    label: 'mae',
+  },
+  {
+    type: 'number',
+    label: 'rmse',
+  },
+  {
+    type: 'number',
+    label: 'rscore',
+  }
+];
