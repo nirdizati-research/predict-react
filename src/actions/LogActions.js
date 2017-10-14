be82ed8e36a33594c78a4ebc6033ae92b49f7cb7
Tõnis Kasekamp <toniskasekamp@gmail.com>
2017-10-14 16:58:12 +0300

    Add tests (#2)
    * FetcState test
    
    * LogListCard test
    
    * Various tests
    
    * Create checkboxgroup
    
    Wrapper for regression and classification method selection
    
    * Threshold test
    
    * Action tests
    
    * Update react versions
    
    Get rid of some react enzyme warn message
    
    * Add Travis CI
    
    * Fix lint errors

--- src/actions/LogActions.js
+++ src/actions/LogActions.js
@@ -23,4 +23,3 @@ export const logInfoRetrieved = createPayloadForwardingAction(LOG_INFO_RETRIEVED
 
 export const LOG_INFO_FAILED = 'LOG_INFO_FAILED';
 export const logInfoFailed = createPayloadForwardingAction(LOG_INFO_FAILED);
-
