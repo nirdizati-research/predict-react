3d844a9ad052e1fc5ddd4a7c6381564856985441
Tõnis Kasekamp <tonis.kasekamp@salemove.com>
2017-10-16 12:40:25 +0300

    Middleware tests

---
+++ tests/middlewares/TrainingMiddleware.test.js
@@ -0,0 +1,29 @@
+/**
+ * Created by tonis.kasekamp on 10/16/17.
+ */
+import {push} from 'react-router-redux';
+import {trainingSucceeded} from '../../src/actions/JobActions';
+import trainingMiddleware from '../../src/middlewares/TrainingMiddleware';
+
+const store = {
+  dispatch: jest.fn()
+};
+const next = jest.fn();
+
+describe('TrainingMiddleware', () => {
+  afterEach(() => {
+    store.dispatch.mockClear();
+  });
+
+  it('navigates to jobStatuswhen training succeeded', () => {
+    trainingMiddleware(store)(next)(trainingSucceeded());
+
+    expect(store.dispatch.mock.calls[0][0]).toEqual(push('/jobStatus'));
+  });
+
+  it('does nothing for other actions', () => {
+    trainingMiddleware(store)(next)({type: 'AA'});
+
+    expect(store.dispatch.mock.calls.length).toEqual(0);
+  });
+});
