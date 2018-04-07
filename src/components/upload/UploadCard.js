70468111a4c5951bd0f5ff45cbeb94cb581a3ad4
Tõnis Kasekamp <tonis.kasekamp@salemove.com>
2018-04-07 14:11:58 +0300

    Upload page descriptions

--- src/components/upload/UploadCard.js
+++ src/components/upload/UploadCard.js
@@ -6,7 +6,10 @@ const UploadCard = () => {
   return <Card className="md-block-centered">
     <CardTitle title="Upload"/>
     <CardText>
+      <h3>After upload create a Split with this log!</h3>
+
       Upload a single log file.
+      Log parameters for the charts will be calculated during the upload and this process may take time. Remain patient!
       <ServerUpload/>
     </CardText>
   </Card>;
