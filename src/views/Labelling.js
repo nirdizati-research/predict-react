e0c39a9efa5adfb1a34fe036f4928a22b8d84196
Tõnis Kasekamp <tonis.kasekamp@salemove.com>
2018-05-12 16:37:20 +0300

    Fix labelling chart prefix

--- src/views/Labelling.js
+++ src/views/Labelling.js
@@ -70,7 +70,7 @@ class Labelling extends Component {
       const job = this.state.clickedJobId ?
         this.props.jobs.filter((job) => job.id === this.state.clickedJobId)[0] : this.props.jobs[0];
       const a = job ? job.id : '';
-      const prefix = job ? job.config.prefix_length : '';
+      const prefix = job ? job.config.encoding.prefix_length : '';
       return <div className="md-cell md-cell--6">
         <BarChartCard data={job ? job.result : {}}
                       cardTitle={`Labels for task ${a}, prefix length ${prefix}`}
