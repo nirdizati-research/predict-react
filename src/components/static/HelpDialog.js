ee3ad37260aee86b8d3cd5f2766aee57aa89f609
Tõnis Kasekamp <tonis.kasekamp@salemove.com>
2018-05-29 21:42:04 +0300

    Add more help texts

--- src/components/static/HelpDialog.js
+++ src/components/static/HelpDialog.js
@@ -5,6 +5,13 @@ import {DialogContainer} from 'react-md';
 
 const contentProps = {id: 'scrolling-content-dialog-content'};
 
+const thesisUrl = 'https://comserv.cs.ut.ee/ati_thesis/datasheet.php?id=62131&year=2018';
+
+const actions = [{
+  label: 'Thesis Documentation',
+  primary: true,
+  onClick: () => window.open(thesisUrl),
+}];
 
 const HelpDialog = (props) => {
   const paragraphs = props.texts.map((text, i) => (
@@ -12,6 +19,12 @@ const HelpDialog = (props) => {
       {text}
     </p>
   ));
+
+
+  const header = <p key={0} className={'md-caption'}>This documentation is based on the Master thesis by Tõnis Kasekamp
+    called "A Web Application to Support Researchers in Predictive Process Monitoring Tasks". The thesis contains better
+    documentation about the functionality, so look there for a full explanation about the configuration
+    options.</p>;
 
   return <DialogContainer
     id="help-dialog"
@@ -21,7 +34,9 @@ const HelpDialog = (props) => {
     contentProps={contentProps}
     width={600}
     focusOnMount={false}
+    actions={actions}
   >
+    {header}
     {paragraphs}
   </DialogContainer>;
 };
