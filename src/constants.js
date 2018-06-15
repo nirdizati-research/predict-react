03317af5b15bb012470a86295141f2770459c0c5
Tõnis Kasekamp <tonis.kasekamp@salemove.com>
2018-06-15 09:00:05 +0300

    Basic websocket implementation
    Connects to server and listens to Job object updates.

--- src/constants.js
+++ src/constants.js
@@ -4,11 +4,20 @@
 
 const DEV_SERVER = 'http://localhost:8000';
 const PROD_SERVER = 'http://193.40.11.46';
+const DEV_WEBSOCKET = {
+  port: 8000,
+  host: 'localhost'
+};
+const PROD_WEBSOCKET = {
+  host: '193.40.11.46'
+};
 export let SERVER_URL = DEV_SERVER;
+export let WEBSOCKET_URL = `ws://${DEV_WEBSOCKET.host}:${DEV_WEBSOCKET.port}/ws/default?subscribe-broadcast`;
 
 export const chooseServer = () => {
   if (process.env.NODE_ENV === 'production') {
     SERVER_URL = PROD_SERVER;
+    WEBSOCKET_URL = `ws://${PROD_WEBSOCKET.host}/ws/default?subscribe-broadcast`;
   }
   // SERVER_URL = PROD_SERVER;
 };
