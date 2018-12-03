e004e3fdda1c5741adbee21bb6d31d371b916b4f
Williams Rizzi <rizzi.williams@me.com>
2018-12-03 13:01:44 +0100

    this commit might be an error

--- src/__tests__/components/WalkTrough.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {shallow} from 'enzyme';
-import WalkThrough from '../../components/WalkThrough';
-
-it('renders', () => {
-  const element = shallow(<WalkThrough/>);
-  expect(element).toBeDefined();
-});
-
--- src/setupTests.js
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-Enzyme.configure({adapter: new Adapter()});
