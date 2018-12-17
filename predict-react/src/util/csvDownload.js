a92e65475f4977439c20ce5cab8b30e55b0a90cb
Williams Rizzi <rizzi.williams@me.com>
2018-12-17 17:17:07 +0100

    updated gitignore and removed a wrong copy of the project

--- predict-react/src/util/csvDownload.js
+++
@@ -1,5 +0,0 @@
-// Transform data to CSV format
-export const makeCSV = (columns, data) => {
-  const header = columns.map((el) => el.label).join() + '\n';
-  return header + data.map((row) => row.join()).join('\n');
-};
