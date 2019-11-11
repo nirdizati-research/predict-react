f30588d6a9b3f14f16e2165725e9191c38a94e33
stebranchi <ste.branchi@gmail.com>
2019-11-11 10:34:12 +0100

    Pushing runtimetable changes with traces rows before merging

--- src/components/runtime/RuntimeTable.js
+++ src/components/runtime/RuntimeTable.js
@@ -1,42 +1,64 @@
 import React, {PureComponent} from 'react';
 import {DataTable, TableBody, TableColumn, TableHeader, TablePagination, TableRow} from 'react-md/lib/DataTables/index';
 import PropTypes from 'prop-types';
-import {tracePropType} from '../../propTypes';
+import {jobRunPropType} from '../../propTypes';
+import JsonHolder from '../validation/JsonHolder';
 
 /* eslint-disable camelcase */
-/* eslint-disable max-len */
 class RuntimeTable extends PureComponent {
   constructor(props) {
     super(props);
-    this.state = {slicedData: []};
+    this.state = {slicedData: this.props.jobs.slice(0, 10)};
   }
 
   handlePagination(start, rowsPerPage) {
-    this.setState({slicedData: this.props.traces.slice(start, start + rowsPerPage)});
-  }
-
-  compareTraces(prevtraces, traces) {
-    if (prevtraces.length !== traces.length) return true;
-    for (let i = 0; i < prevtraces.length; i=i+1) {
-      if ((prevtraces[i].last_event !== traces[i].last_event) || (prevtraces[i].completed !== traces[i].completed) ||
-        (prevtraces[i].n_events !== traces[i].n_events)) return true;
-    }
-    return false;
+    this.setState({slicedData: this.props.jobs.slice(start, start + rowsPerPage)});
   }
 
   componentDidUpdate(prevProps) {
-    if (this.compareTraces(prevProps.traces, this.props.traces)) {
-      this.setState({slicedData: this.props.traces.slice(0, 10)});
+    if (prevProps.jobs.length !== this.props.jobs.length) {
+      this.setState({slicedData: this.props.jobs.slice(0, 10)});
     }
+  }
+
+  createTableData() {
+      this.props.jobs.map((job) => {
+          job.case_id.map((trace_id) => {
+              return {
+                  trace_id: trace_id,
+                  events: job.event_number[trace_id],
+                  configuration: job.configuration,
+                  prediction: job.results[trace_id],
+                  gold_value: job.gold_value[trace_id],
+                  error: job.error,
+              }
+          });
+      })
+  }
+
+  componentDidMount() {
+    const intervalId = setInterval(() => {
+      this.props.onRequestJobs();
+      this.setState({slicedData: this.createTableData().slice(0, 10)});
+    }, 5000);
+    this.setState({intervalId: intervalId});
+  }
+
+
+  componentWillUnmount() {
+    clearInterval(this.state.intervalId);
   }
 
   getHeaderColumns() {
     const headers =
-      ['Trace Name', 'Completed', 'Events Elapsed', 'Start Time', 'Latest event time',
-          'Regression Prediction', 'Classification Prediction', 'Time Series Prediction Prediction'];
+      ['Trace_id', 'Events', 'Configuration', 'Prediction', 'Actual value', 'error'];
 
     return headers.map((header) => {
-        return <TableColumn key={header}> {header}</TableColumn>;
+        let grow = false;
+        if (header === 'Configuration') {
+          grow = true;
+        }
+        return <TableColumn key={header} grow={grow}> {header}</TableColumn>;
       }
     );
   }
@@ -49,28 +71,26 @@ class RuntimeTable extends PureComponent {
         </TableRow>
       </TableHeader>
       <TableBody>
-          {this.state.slicedData.map( // TODO: update to use timeSeriesPrediction
-          ({id, name, completed, n_events, first_event, last_event, reg_results, class_results, class_actual, duration, inter_result}) => (
-            <TableRow key={id} selectable={false}>
-              <TableColumn>{name}</TableColumn>
-              <TableColumn>{completed ? 'True' : 'False'}</TableColumn>
-              <TableColumn>{n_events}</TableColumn>
-              <TableColumn>{new Date(first_event).toLocaleString()}</TableColumn>
-              <TableColumn>{new Date(last_event).toLocaleString()}</TableColumn>
-              <TableColumn>{JSON.stringify(reg_results)}</TableColumn>
-              <TableColumn>{JSON.stringify(class_results)}</TableColumn>
+        {this.state.slicedData.map(
+          ({trace_id, events, configuration, prediction, gold_value}) => (
+            <TableRow key={trace_id} selectable={false}>
+              <TableColumn numeric>{trace_id}</TableColumn>
+              <TableColumn>{JSON.stringify(events)}</TableColumn>
+              <TableColumn grow><JsonHolder data={configuration}/></TableColumn>
+              <TableColumn>{JSON.stringify(prediction)}</TableColumn>
+              <TableColumn>{JSON.stringify(gold_value)}</TableColumn>
             </TableRow>
           ))}
       </TableBody>
-      <TablePagination rows={this.props.traces.length} rowsPerPageLabel={'Rows per page'}
+      <TablePagination rows={this.props.jobs.length} rowsPerPageLabel={'Rows per page'}
                        onPagination={this.handlePagination.bind(this)}/>
     </DataTable>);
   }
 }
 
 RuntimeTable.propTypes = {
-  traces: PropTypes.arrayOf(tracePropType).isRequired,
-  onRequestTraces: PropTypes.func.isRequired,
+  jobs: PropTypes.arrayOf(jobRunPropType).isRequired,
+  onRequestJobs: PropTypes.func.isRequired,
 };
 
 export default RuntimeTable;
