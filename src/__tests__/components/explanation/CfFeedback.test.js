2fb0e5b7d69b00296aba39ba3ff27c6a60c02e99
Musacca <mmmusabir@gmail.com>
2020-04-19 16:14:33 +0300

    Confusion Matrix feedback and retrain test

---
+++ src/__tests__/components/explanation/CfFeedback.test.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import CircularProgress from 'react-md/lib/Progress/CircularProgress';
+import {retrainResult, cfFeedbackResult, uniqueEncodedDecodedValues} from '../../../../stories/Explanation';
+import CfFeedback from '../../../components/explanation/CfFeedback';
+import RetrainResultTable from '../../../components/explanation/RetrainResultTable';
+import {TextField, SelectField} from 'react-md';
+import CfFeedbackResulttTable from '../../../components/explanation/CfFeedbackResulttTable';
+import {getUniqueFeatureValues, getFeatureNames, encodePatternsForDropdown} from '../../../util/dataReducers';
+
+const onSubmitTopK = jest.fn();
+const onSubmitFeatureNamesAndValues = jest.fn();
+
+describe('CfFeedback result', () => {
+    it('All data loaded', () => {
+        const element = shallow(<CfFeedback
+            jobId = {1}
+            cfFeedbackValue={cfFeedbackResult}
+            isCfFeedbackValuesLoaded={true}
+            isRetrainValuesLoaded={true}
+            isEncodedUniqueValuesLoaded={true}
+            retrainValue={retrainResult}
+            featureNames={getFeatureNames(uniqueEncodedDecodedValues)}
+            patterns={encodePatternsForDropdown(cfFeedbackResult[1])}
+            featureValues={getUniqueFeatureValues(uniqueEncodedDecodedValues)}
+            onSubmitTopK={onSubmitTopK}
+            onSubmitFeatureNamesAndValues={onSubmitFeatureNamesAndValues}/>);
+        expect(element).toBeDefined();
+        element.setState({numberOfDropdownFeatures: 1});
+        element.setState({numberOfDropdownPatterns: 1});
+
+        expect(element.find(TextField).length).toBe(1);
+        expect(element.find(CfFeedbackResulttTable).length).toBe(1);
+        expect(element.find(RetrainResultTable).length).toBe(1);
+        expect(element.find(TextField).at(0).props().min).toBe(0);
+        expect(element.find(SelectField).length).toBe(3);
+        expect(element.find(CircularProgress).length).toBe(0);
+    });
+
+     it('None of the data loaded', () => {
+        const element = shallow(<CfFeedback
+            jobId = {1}
+            cfFeedbackValue={cfFeedbackResult}
+            isCfFeedbackValuesLoaded={false}
+            isRetrainValuesLoaded={false}
+            isEncodedUniqueValuesLoaded={false}
+            retrainValue={retrainResult}
+            featureNames={getFeatureNames(uniqueEncodedDecodedValues)}
+            patterns={encodePatternsForDropdown(cfFeedbackResult[1])}
+            featureValues={getUniqueFeatureValues(uniqueEncodedDecodedValues)}
+            onSubmitTopK={onSubmitTopK}
+            onSubmitFeatureNamesAndValues={onSubmitFeatureNamesAndValues}/>);
+        expect(element).toBeDefined();
+
+        element.setState({numberOfDropdownFeatures: 1});
+        element.setState({numberOfDropdownPatterns: 1});
+
+        expect(element.find(TextField).length).toBe(1);
+        expect(element.find(CfFeedbackResulttTable).length).toBe(1);
+        expect(element.find(RetrainResultTable).length).toBe(1);
+        expect(element.find(TextField).at(0).props().min).toBe(0);
+        expect(element.find(SelectField).length).toBe(3);
+        expect(element.find(CircularProgress).length).toBe(3);
+     });
+});
